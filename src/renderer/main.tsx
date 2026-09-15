b345c67977642d5c373d834c8962d4ca0375a17c
ahmeterdgn <ahmeterdgn6@gmail.com>
2026-09-16 00:34:17 +0300

    fix: import highlight.js github-dark theme for code syntax coloring

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import "highlight.js/styles/github-dark.css";
 import App from "./App.js";
 
 document.documentElement.classList.add("dark");
